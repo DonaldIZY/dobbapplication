07cfcb405bf340707b53576a54d2f3d0b3e2ff76
DonaldIZY <122298248+DonaldIZY@users.noreply.github.com>
2023-03-12 19:06:56 +0000

    12/03/2023 19:04

---
+++ static/analytics/js/clientele.js
@@ -0,0 +1,47 @@
+/* jshint esversion: 6 */
+function evoFormat(val) {
+  'use strict';
+  if(val === 0) {
+    val = '-';
+  }
+  return val;
+}
+
+function setColor(color) {
+  'use strict';
+  return {
+    color: function (params) {
+      if (params.dataIndex === 0) {
+        return '#7b7e7e';
+      } else {
+        return color;
+      }
+    }
+  };
+}
+
+let url = window.location.href;
+/* jshint ignore:start */
+console.log(url);
+/* jshint ignore:end */
+
+
+
+
+// =====================================================================================================================
+
+document.getElementById('univers').addEventListener('change', function (qualifiedName, value) {
+  // code à exécuter lorsque la valeur change
+  // document.getElementById("header").innerHTML = this.value;
+
+  let elements = document.getElementsByClassName('graph-title');
+  for (let ele of elements) {
+    ele.innerHTML = this.value;
+  }
+
+  getData(this.value);
+});
+
+document.getElementById('select-date').addEventListener('change', function (qualifiedName, value) {
+  console.log(this);
+});
